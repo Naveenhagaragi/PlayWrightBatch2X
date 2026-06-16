0d91fdb6e863981eb1682a553b758e79493f419c
naveenhagaragi <naveenhagaragi@gmail.com>
2026-06-16 13:11:42 +0530

    Add Chapter 16 Callback and Chapter 17 Promise content with updates to Chapter 15

---
+++ Chapter_15_2D_Array/140_REAL.js
@@ -0,0 +1,61 @@
+let testmatrix = [
+    ["Login", "pass", 200],
+    ["Search", "fail", 404],
+    ["checkout", "pass", 180],
+    ["payment", "fail", 501]
+];
+
+//How many test cases have we executed.
+// how many test cases are passed
+// What is the status codes that you got for failed test cases?
+
+// Print 2d array
+for (let i = 0; i < testmatrix.length; i++) {
+    for (let j = 0; j < testmatrix[i].length; j++) {
+        console.log(testmatrix[i][j]);
+    }
+    console.log();
+}
+
+
+
+//another way to print the 2d array
+for (row of testmatrix) {     // row & cell are variable names, i.e x,y
+    for (cell of row) {
+        console.log(cell + " ");
+    }
+    console.log();
+}
+
+// another way to print 2d array usong forEach
+testmatrix.forEach(row => {
+    row.forEach(
+        cell => process.stdout.write(cell + " ")
+    );
+    console.log();
+});
+
+
+// How many test cases have we executed.
+console.log(`Test cases executed: ${testmatrix.length}`);
+
+// how many test cases are passed
+let pass = 0; let fail = 0;
+for (let i = 0; i < testmatrix.length; i++) {
+
+    if (testmatrix[i].includes("pass")) {
+        console.log(testmatrix[i]);
+        pass++;
+    }
+    else fail++;
+}
+
+console.log(`Passed test cases: ${pass}`);
+console.log(`Failed test cases: ${fail}`);
+
+// What is the status codes that you got for failed test cases?
+for (let i = 0; i < testmatrix.length; i++) {
+    if (testmatrix[i].includes("fail")) {
+        console.log(testmatrix[i][2]);
+    }
+}
