ef8e2e0252b66298326a7851ecdaea734f5488a5
naveenhagaragi <naveenhagaragi@gmail.com>
2026-07-16 00:01:09 +0530

    Sync latest Playwright config fixes

---
+++ Chapter_32_Playwright_Fundamentals/tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify TTACART Title', async ({ page }) => {
+  await page.goto('https://app.thetestingacademy.com/playwright/ttacart/');
+  await expect(page).toHaveTitle("TTACart - Login");
+
+});
